cda8a7a88fedc53f81fb87e7809960fe984b7181
Ryan Bevan <ryan5905@tamu.edu>
2020-02-04 12:38:20 -0600

    basic sort working

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,6 @@
+# Class MoviesController
 class MoviesController < ApplicationController
-
+  helper_method :sort_column, :sort_direction
   def movie_params
     params.require(:movie).permit(:title, :rating, :description, :release_date)
   end
@@ -11,7 +12,7 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    @movies = Movie.order(sort_column + " " + sort_direction)
   end
 
   def new
@@ -42,4 +43,12 @@ class MoviesController < ApplicationController
     redirect_to movies_path
   end
 
+  private
+
+  def sort_column
+    Movie.column_names.include?(params[:sort]) ? params[:sort] : 'title'
+  end
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+  end
 end
--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,6 +1,14 @@
 module MoviesHelper
   # Checks if a number is odd:
   def oddness(count)
-    count.odd? ?  "odd" :  "even"
+    count.odd? ? 'odd' : 'even'
+  end
+
+  # Sorts the movies by column:
+  def sortable(column, title = nil)
+    title ||= column.titleize
+    css_class = column == sort_column ? "current #{sort_direction}" : nil
+    direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
+    link_to title, {:sort => column, :direction => direction}, {:class => css_class}
   end
 end
