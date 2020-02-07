d99a0d31e16e6baf79e6090cce3cac3bca1fae25
Ryan Bevan <ryan5905@tamu.edu>
2020-02-07 14:51:09 -0600

    added comments to clarify thoguht process

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -22,12 +22,13 @@ class MoviesController < ApplicationController
       session[:order] = params[:order]
     end
 
+    # create variables to test the state of sessions and parameters
     new_rating_param = !params[:ratings].nil?
     new_order_param = !params[:order].nil?
     session_order = !session[:order].nil?
     session_rating = !session[:ratings].nil?
 
-    # if no new params and existing session, pick up where the user left off
+    # if no new params and existing session, pick up where the user left off. If no session as well, load all movies
     if !new_rating_param && !new_order_param
       if session_rating
         redirect_to movies_path('ratings': session[:ratings], 'order': session[:order])
@@ -40,6 +41,8 @@ class MoviesController < ApplicationController
       # if new param for rating, update with new rating and existing order
     elsif new_rating_param
       @movies = Movie.where(rating: params[:ratings].keys).order(session[:order])
+
+      # load new order with existing rating
     elsif session_rating || session_order
       redirect_to movies_path('ratings': session[:ratings], 'order': session[:order])
     else
